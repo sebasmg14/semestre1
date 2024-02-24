fc868e42c78a59c9423f887e24d24d7ca3917f0c
sebasmg14 <161092992+sebasmg14@users.noreply.github.com>
2024-02-23 19:30:37 -0500

    Add files via upload

---
+++ 1. Ejercicios matematicos.py
@@ -0,0 +1,12 @@
+num1 = 10
+num2 = 20
+
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
